5671ffeec0afb2b486bf0b43c8bb6d287108867f
PricaMilos <prica.milos97@gmail.com>
2022-11-18 10:35:54 +0100

    created application vertical

---
+++ application/src/main/java/com/nttdata/controller/ApplicationController.java
@@ -0,0 +1,7 @@
+package com.nttdata.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApplicationController {
+}
---
+++ application/src/main/java/com/nttdata/entity/ApplicationEntity.java
@@ -0,0 +1,25 @@
+package com.nttdata.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(schema = "recruitment_system", name = "application")
+@Data
+public class ApplicationEntity {
+
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   @Column(name = "id")
+   private int id;
+
+   @Column(name = "creation_date")
+   private Date creationDate;
+
+   @Column(name = "recruiter_comment")
+   private String recruiterComment;
+
+
+}
