53557871caee6a8d75df5d6077b5364b1f763076
Nikola Stanic <nibscompany@gmail.com>
2022-11-24 08:15:16 +0100

    Commit new changes on Form Template entity and on Skill group template entity.

--- template/src/main/java/com/nttdata/recruitmentsystem/template/mapper/SkillGroupTemplateMapper.java
+++ template/src/main/java/com/nttdata/recruitmentsystem/template/mapper/SkillGroupTemplateMapper.java
@@ -1,8 +1,12 @@
 package com.nttdata.recruitmentsystem.template.mapper;
 
 import com.nttdata.recruitmentsystem.template.dto.SkillGroupTemplate;
+import com.nttdata.recruitmentsystem.template.dto.TopicTemplate;
 import com.nttdata.recruitmentsystem.template.entity.SkillGroupTemplateEntity;
 
+import javax.persistence.EntityResult;
+import javax.persistence.FieldResult;
+import javax.persistence.SqlResultSetMapping;
 import java.util.ArrayList;
 
 public class SkillGroupTemplateMapper {
@@ -20,4 +24,22 @@ public class SkillGroupTemplateMapper {
                 .skillGroupName(s.getSkillGroupTemplateName())
                 .build();
     }
+
+    @SqlResultSetMapping(
+            name = "TopicSkillGroupMapping",
+            entities = {
+                    @EntityResult(
+                            entityClass = TopicTemplate.class,
+                            fields = {
+                                    @FieldResult(name = "id", column = "topic_template_id"),
+                                    @FieldResult(name = "topicTemplateName", column = "topic_name"),
+                                    @FieldResult(name = "skillGroupTemplateEntity", column = "fk_skill_group_template"),}),
+
+                    @EntityResult(
+                            entityClass = SkillGroupTemplate.class,
+                            fields = {
+                                    @FieldResult(name = "id", column = "skill_group_template_id"),
+                                    @FieldResult(name = "skillGroupName", column = "skill_group_name"),
+                            })})
+
 }
--- template/src/main/java/com/nttdata/recruitmentsystem/template/repository/TopicTemplateRepository.java
+++ template/src/main/java/com/nttdata/recruitmentsystem/template/repository/TopicTemplateRepository.java
@@ -1,11 +1,16 @@
 package com.nttdata.recruitmentsystem.template.repository;
 
+import com.nttdata.recruitmentsystem.template.dto.SkillGroupTemplate;
+import com.nttdata.recruitmentsystem.template.dto.TopicTemplate;
 import com.nttdata.recruitmentsystem.template.entity.SkillGroupTemplateEntity;
 import com.nttdata.recruitmentsystem.template.entity.TopicTemplateEntity;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityResult;
+import javax.persistence.FieldResult;
+import javax.persistence.SqlResultSetMapping;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,6 +19,11 @@ public interface TopicTemplateRepository extends CrudRepository <TopicTemplateEn
 
     Optional<TopicTemplateEntity> findTopicById(Integer id);
 
-    @Query("SELECT skill.topic FROM SkillGroupTemplateEntity skill JOIN skill.topicTemplateEntities t WHERE skill.skillGroupName = :skillGroupTemplateName")
+
+    @Query("SELECT t.topic_template_id, t.topic_name, t.fk_skill_group_template, " +
+            "s.skill_group_template_id, s.skill_group_name FROM TopicTemplate t " +
+            "JOIN SkillGroupTemplate s ON t.fk_skill_group_template = s.skill_group_template_id")
     List<TopicTemplateEntity> findAllTopicsBySkillGroupTemplateName(String skillGroupTemplateName);
+
+
 }
